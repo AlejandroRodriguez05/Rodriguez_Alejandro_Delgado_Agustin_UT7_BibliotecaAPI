9c13ed24d59274b93c5d7d4642dcc950a33dd3b2
AgustinDelgadoEstevez04 <agustindelgadoestevez@gmail.com>
2025-05-19 19:12:41 +0100

    autor service y controller terminado

--- Delgado_Agustin_Rodriguez_Alejandro_Ut7_BiblioteaApi/src/main/java/com/tuapp/bibliotecaapi/controller/AutorController.java
+++ Delgado_Agustin_Rodriguez_Alejandro_Ut7_BiblioteaApi/src/main/java/com/tuapp/bibliotecaapi/controller/AutorController.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tuapp.bibliotecaapi.controller;
-
+import com.tuapp.bibliotecaapi.model.Autor;
+import com.tuapp.bibliotecaapi.service.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 /**
  *
  * @author chati
  */
+@RestController
+@RequestMapping("/api/v1/autores")
 public class AutorController {
+   private final AutorService autorService;
     
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @GetMapping
+    public List<Autor> getAllAutores() {
+        return autorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Autor> getAutorById(@PathVariable Long id) {
+        return autorService.findById(id);
+    }
+
+    @PostMapping
+    public Autor createAutor(@RequestBody Autor autor) {
+        return autorService.save(autor);
+    }
 }
--- Delgado_Agustin_Rodriguez_Alejandro_Ut7_BiblioteaApi/src/main/java/com/tuapp/bibliotecaapi/service/AutorService.java
+++ Delgado_Agustin_Rodriguez_Alejandro_Ut7_BiblioteaApi/src/main/java/com/tuapp/bibliotecaapi/service/AutorService.java
@@ -3,28 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tuapp.bibliotecaapi.service;
+
 import com.tuapp.bibliotecaapi.model.Autor;
 import com.tuapp.bibliotecaapi.repository.AutorRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 /**
  *
  * @author chati
  */
 @Service
 public class AutorService {
+
     @Autowired
     private AutorRepository autorRepository;
-    
-    public List<Autor> findAll(){
-    return autorRepository.findAll();
+
+    public List<Autor> findAll() {
+        return autorRepository.findAll();
     }
-    public Optional<Autor> findById(Long id){
-    return autorRepository.findById(id);
+
+    public Optional<Autor> findById(Long id) {
+        return autorRepository.findById(id);
     }
-    public Autor save(Autor autor){
-    return autorRepository.save(autor);
+
+    public Autor save(Autor autor) {
+        return autorRepository.save(autor);
     }
 }
